e00f8c637705c4ef62fc03317184b5de953113c4
aleons <thealeons@gmail.com>
2021-08-27 13:52:05 +0300

    CR

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,15 +2,15 @@ import { Select } from './Component/Select/Select';
 
 function App() {
   const optionsData = [
-                    {
-                      text:"React",
-                      value:"react",
-                    },
-                    {
-                      text:"Vue",
-                      value:"vue",
-                    },
-                  ];
+                        {
+                          text:"React",
+                          value:"react",
+                        },
+                        {
+                          text:"Vue",
+                          value:"vue",
+                        },
+                      ];
   return (
     <div className="App">
       <Select optionsData = {optionsData}/>
